a3c9178ee62e83c5fba65c4f6bc583d8ac685ff9
Brayan Perez <brayanperezbuitrago@gmail.com>
2024-10-28 20:59:01 -0500

    Fixed base in vite config to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/calc-horas-extra/',
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': '/src'
+      // '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
